0862e2d17bf89185cc3392645ebabb2495e7f3a2
gab <gabrielfontes321@gmail.com>
2021-09-15 21:31:18 -0300

    Exercicio do dia 6.1

---
+++ fundamentos/fundamentos/6.Html & CSS - Forms/dia-1-html-css-forms/script.js
@@ -0,0 +1,29 @@
+let state = document.getElementById('state-input');
+let stateOpt = ['AL', 'SP', 'MG', 'BH', 'AM'];
+let button = document.getElementById('default-button');
+
+function optGenerator() {
+    for (let i = 0; i < stateOpt.length; i += 1) {
+        let opt = document.createElement('option');
+        state.appendChild(opt).value = stateOpt[i];
+        state.appendChild(opt).innerText = stateOpt[i];
+    }
+}
+optGenerator();
+
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+});
+
+let clearButton = document.getElementById('clear');
+
+clearButton.addEventListener('click', () => {
+  let inputs = document.querySelectorAll('input');
+  let textArea = document.querySelector('.resume')
+  for (let i = 0; i < inputs.length; i+= 1) {
+    let input = inputs[i];
+    input.value = '';
+    textArea.value = '';
+    
+  }
+});
