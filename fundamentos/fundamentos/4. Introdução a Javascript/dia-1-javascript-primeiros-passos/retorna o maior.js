92aadd78292ded4024dce4849333e51b3c8fef7f
gab <gabrielfontes321@gmail.com>
2022-03-11 16:11:54 -0300

    .

--- fundamentos/fundamentos/4. Introdução a Javascript/dia-1-javascript-primeiros-passos/retorna o maior.js
+++
@@ -1,9 +0,0 @@
-const a = 2
-const b = 5
-
-if (a > b) {
-    console.log(a);
-}
-else {
-    console.log(b);
-}
--- fundamentos/fundamentos/4. Introdução a Javascript/dia-2-javascript-array-e-loop-for/Imprime os valores do array.js
+++
@@ -1,6 +0,0 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let numberlist
-
-for (numberlist of numbers) {
-    console.log(numberlist);
-}
