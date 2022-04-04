d7fa126614018f3531cbb3894b856a19559f8d7b
gab <gabrielfontes321@gmail.com>
2022-04-04 16:46:00 -0300

    feat: exercicios dia 23.4

---
+++ modulos/back-end/bloco-23-Node.js-Camada-de-Serviço-e-Arquitetura-Rest-e-Restful/dia-4-testando-as-camadas/fixacao/models/connection.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'gab',
+    password: 'Gabriel.40028922',
+    database: 'model_example'
+});
+
+module.exports = connection;
